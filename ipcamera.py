ad12e762d354737afc8244f2aaa9e5084b3339c1
TienTruong98 <tientt1938@gmail.com>
2020-12-10 07:00:04 +0700

    add dafang cammera rtsp configuration file - fix when pressing q all thread will stop in ipcamera.py - add face mask detection in ipcamera.py

--- ipcamera.py
+++ ipcamera.py
@@ -2,38 +2,51 @@ import cv2
 import queue
 import time
 import threading
+import os
 import detect
-import sys
+
+
+
 q=queue.Queue()
+
 
 def Receive():
     print("start Reveive")
-    cap = cv2.VideoCapture("rtsp://admin:admin@192.168.1.105:8554/unicast")
+    cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.43.245:8554/unicast")
     ret, frame = cap.read()
     q.put(frame)
-    while ret:
+    while ret and p2.is_alive():
         ret, frame = cap.read()
         q.put(frame)
-
+    cap.release()
 
 
 def Display():
-    print("Start Displaying")
-    while True:
+     print("Start Displaying")
+     a = False
+     while True:
         if q.empty() !=True:
             frame=q.get()
             if frame is not None:
-                idxs, boxes, classIDs, confidences = detect.detect(frame)
-                frame = detect.draw_frame(idxs, boxes, classIDs, confidences, frame)
+                if a:
+                    idxs, boxes, classIDs, confidences = detect.detect(frame)
+                    frame2 = detect.draw_frame(idxs, boxes, classIDs, confidences, frame)
+                    cv2.imshow("frame2", frame2)
+                    a = False
                 cv2.imshow("frame1", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            sys.exit()
+            break
+        if cv2.waitKey(1) == ord('x'):
+            a = True
+
+p1 = threading.Thread(target=Receive)
+p2 = threading.Thread(target=Display)
 
 if __name__=='__main__':
-    p1=threading.Thread(target=Receive)
-    p2 = threading.Thread(target=Display)
-    threads = [p1, p2]
+
     p1.start()
     p2.start()
-    p1.join(p2)
+
+
+
 
